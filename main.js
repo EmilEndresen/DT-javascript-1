eb101f55a9b4d707004d9bb8089e76326b3f3b73
Emil Endresen <emil@jobloop.no>
2023-11-27 14:22:26 +0100

    initial commit

---
+++ main.js
@@ -0,0 +1,35 @@
+console.log("Hello World")
+
+//Kommentar
+
+/*
+Multiline
+Kommentar
+*/
+
+//Variabler
+
+let a = 2
+let b = 15
+
+console.log(a + b)
+
+// String
+let firstName = "Emil Johan"
+console.log(firstName)
+
+// Integer/Number
+let x = 3 + 5
+console.log(x)
+
+// Boolean
+let raining = false
+console.log(raining)
+
+// Array
+let favoriteFoods = ["Kebab", "Carbonara", "Pizza"]
+console.log(favoriteFoods)
+
+//undefined
+let y
+console.log(y)
