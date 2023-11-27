e472672662d8a6f5c52b42cd97ce75bce3bcaebf
Emil Endresen <emil@jobloop.no>
2023-11-27 14:58:35 +0100

    første timen

--- main.js
+++ main.js
@@ -11,25 +11,58 @@ Kommentar
 
 let a = 2
 let b = 15
+let c = 2
+let d = 2
+let e = 2
+let food = "beef"
+const comparison = a < b
+c++
+d += 2
+e--
 
+console.log(c)
+console.log(d)
+console.log(e)
 console.log(a + b)
+console.log(comparison)
+
+//DATA TYPES
 
 // String
-let firstName = "Emil Johan"
-console.log(firstName)
+let firstName = `Emil Johan`
+console.log(`Hello, my name is ${firstName}`)
 
 // Integer/Number
 let x = 3 + 5
 console.log(x)
+
+// Float
+let float = 3.14
+console.log(float)
 
 // Boolean
 let raining = false
 console.log(raining)
 
 // Array
-let favoriteFoods = ["Kebab", "Carbonara", "Pizza"]
+let favoriteFoods = ["Kebab", "Carbonara", "Pizza", food]
 console.log(favoriteFoods)
+
+let fruits = ["Kiwi", "Apple", "Pear", "Melon", "Mango", "Orange"]
+fruits[3] = `Cherries`
+console.log(fruits)
 
 //undefined
 let y
 console.log(y)
+
+//Objects
+
+let myObject = {
+   firstName: "Guttorm",
+   lastName: "Guttormsen",
+   age: 43,
+   eMail: "guttorm.guttormsen@gmail.com"
+}
+
+console.log(myObject.firstName)
